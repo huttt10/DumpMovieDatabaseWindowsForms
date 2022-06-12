97146c4f16aa7f065b11778c9119e7b5b27ac9f2
Jakub Cink <jakub.blik.cink@gmail.com>
2022-06-12 16:19:24 +0200

    Last 1.1

---
+++ MovieDatabaseWindowsForms/Migrations/20220612141855_Last.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MovieDatabaseWindowsForms.Migrations
+{
+    public partial class Last : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
