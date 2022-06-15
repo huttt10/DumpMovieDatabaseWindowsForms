a0b18ecde1e488b86ff9bbffe9d06ff9a42ceea9
Jakub Cink <jakub.blik.cink@gmail.com>
2022-06-15 18:33:16 +0200

    Create test of Form1

---
+++ MovieDatabaseWindowsFormsTests/Form1.cs
@@ -0,0 +1,15 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MovieDatabaseWindowsFormsTests
+{
+    [TestClass]
+    public class Form1
+    {
+        // Name-> 
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+        }
+    }
+}
