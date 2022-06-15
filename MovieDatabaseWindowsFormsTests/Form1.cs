9720bcc5cf10ab6aee04caf49e4355d9c63430f9
Jakub Cink <jakub.blik.cink@gmail.com>
2022-06-15 18:53:46 +0200

    Add comment

--- MovieDatabaseWindowsFormsTests/Form1.cs
+++ MovieDatabaseWindowsFormsTests/Form1.cs
@@ -2,13 +2,20 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace MovieDatabaseWindowsFormsTests
 {
+    // Name -> Name of class + "Tests"
     [TestClass]
-    public class Form1
+    public class Form1Tests
     {
-        // Name-> 
+        // Name-> NameOfMethod which is tested _ Scenario _ ExpectedBehavior    Example - CanBeCancelled_UserIsAdmin_ReturnTrue
         [TestMethod]
         public void TestMethod1()
         {
+            // Arrange - 
+
+            // Act
+
+            // Assert
+
 
         }
     }
